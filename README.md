351c84f5b6ded66b7e4fc42bad70139d36373984
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-05-07 20:15:56 -0700

    Everything

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+Eric Chen
+No Partner
+
+expose: https://hundred-foot-long-sweet-potato.github.io/Lab5_Starter/
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,59 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+const jsConfetti = new JSConfetti();
 function init() {
-  // TODO
+  //Get all the required variables
+  const selectEle = document.getElementById("horn-select");
+  const imgSelect = document.getElementsByTagName("img");
+  const audioEle = document.getElementsByTagName("audio");
+  const playButton = document.getElementsByTagName("button");
+  const soundEle = document.getElementById("volume");
+  let partyHorn = false;
+
+  selectEle.addEventListener("change",(event)=> {
+    //Set image based on event value
+    if (event.target.value == "air-horn")
+    {
+      imgSelect[0].src = "assets/images/air-horn.svg";
+      audioEle[0].src = "assets/audio/air-horn.mp3";
+      partyHorn = false;
+    }else if (event.target.value == "car-horn")
+    {
+      imgSelect[0].src = "assets/images/car-horn.svg";
+      audioEle[0].src = "assets/audio/car-horn.mp3";
+      partyHorn = false;
+    }else if (event.target.value == "party-horn")
+    {
+      imgSelect[0].src = "assets/images/party-horn.svg";
+      audioEle[0].src = "assets/audio/party-horn.mp3";
+      partyHorn = true;
+    }
+  });
+
+  soundEle.addEventListener("change", (event)=>{
+    if (event.target.value == 0)
+    {
+      imgSelect[1].src = "assets/icons/volume-level-0.svg";
+    }else if (event.target.value < 33)
+    {
+      imgSelect[1].src = "assets/icons/volume-level-1.svg";
+    }else if (event.target.value < 67)
+    {
+      imgSelect[1].src = "assets/icons/volume-level-2.svg";
+    }else
+    {
+      imgSelect[1].src = "assets/icons/volume-level-3.svg";
+    }
+    audioEle[0].volume = event.target.value / 100;
+  });
+
+  //Sets the button to play on click
+  playButton[0].addEventListener("click",()=>{
+    audioEle[0].play();
+    if (partyHorn)
+    {
+      jsConfetti.addConfetti();
+    }
+  });
 }
